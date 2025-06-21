ec4f14625c227ea3de080fe6bace7da2c2ad5696
jawahendy <50572163+jawahendy@users.noreply.github.com>
2025-06-21 22:01:25 +0700

    implement animation every section

--- src/components/Home/Mobile.tsx
+++ src/components/Home/Mobile.tsx
@@ -5,28 +5,43 @@ import { ArrowUpRight } from 'lucide-react';
 import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
 import { ourClientData, Skills, Skills2 } from '@/constants/data/Service';
 import Autoplay from 'embla-carousel-autoplay';
-
+import { m } from 'framer-motion';
 
 const Mobile = () => {
     return (
         <div className="grid sm:hidden grid-cols-3 grid-rows-9 gap-4 p-3 max-w-screen-2xl min-h-screen w-full overflow-y-auto max-h-screen">
-            <div className="row-span-2 col-span-3 bg-slate-600 rounded-2xl h-52 bg-white/20 backdrop-blur-md border border-white/30 flex flex-col items-center justify-center">
+            <m.div
+                initial={{ opacity: 0, y: 40 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.7, ease: 'easeOut' }}
+                className="row-span-2 col-span-3 bg-slate-600 rounded-2xl h-52 bg-white/20 backdrop-blur-md border border-white/30 flex flex-col items-center justify-center"
+            >
                 <div className="w-20 h-20 rounded-full overflow-hidden border-4 border-white mb-2">
                     <Image src="/image/PP.jpg" alt="Hendy Nurfrianto" width={80} height={80} className="object-cover w-full h-full" />
                 </div>
                 <div className="text-white font-bold text-lg">Hendy Nurfrianto</div>
                 <div className="text-white text-xs opacity-80 px-5 text-center">I&apos;m a front end developer with 6 years of experience. and still more</div>
-            </div>
-            <div className="col-span-3 row-span-2 row-start-3 bg-slate-600 rounded-2xl mt-12 h-52 bg-white/20 backdrop-blur-md border border-white/30 relative flex flex-col items-center justify-center px-4 pt-4 pb-2">
+            </m.div>
+            <m.div
+                initial={{ opacity: 0, y: 40 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.7, ease: 'easeOut', delay: 0.1 }}
+                onClick={() => window.open('https://drive.google.com/drive/folders/1Y2T8prWNpxZ9nxEz6mHjsLIhL_2P1Feg?usp=sharing', '_blank')}
+                className="hover:cursor-pointer col-span-3 row-span-2 row-start-3 bg-slate-600 rounded-2xl mt-12 h-52 bg-white/20 backdrop-blur-md border border-white/30 relative flex flex-col items-center justify-center px-4 pt-4 pb-2"
+            >
                 {/* Arrow Up Right Icon */}
                 <span className="absolute top-1 right-1 bg-white/30 backdrop-blur-md rounded-full p-1 flex items-center justify-center">
-                    {/* Replace with your ArrowUpRight icon import */}
                     <ArrowUpRight className='w-4 h-4 text-white' />
                 </span>
                 <h2 className="text-2xl font-bold text-white text-center w-full z-10 mb-2">About Me</h2>
                 <p className="text-white/80 text-sm text-center">I enjoy building interactive, user-friendly web experiences, mobile apps and I always eager to learn new technologies to improve my craft.</p>
-            </div>
-            <div className="col-span-3 row-span-2 row-start-5 bg-slate-600 rounded-2xl mt-24 h-52 bg-white/20 backdrop-blur-md border border-white/30 relative flex flex-col items-center justify-center px-2 py-4">
+            </m.div>
+            <m.div
+                initial={{ opacity: 0, y: 40 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.7, ease: 'easeOut', delay: 0.2 }}
+                className="col-span-3 row-span-2 row-start-5 bg-slate-600 rounded-2xl mt-24 h-52 bg-white/20 backdrop-blur-md border border-white/30 relative flex flex-col items-center justify-center px-2 py-4"
+            >
                 <h3 className="text-xl font-bold text-white mb-2 mt-3 text-center w-full z-10">Working Experiance</h3>
                 {/* Client Carousel */}
                 <div className="w-full flex-1 flex items-center justify-center">
@@ -62,8 +77,13 @@ const Mobile = () => {
                         </Carousel>
                     ))}
                 </div>
-            </div>
-            <div className="col-span-3 row-span-2 row-start-7 bg-slate-600 rounded-2xl mt-36 h-60 bg-white/20 backdrop-blur-md border border-white/30 relative flex flex-col items-center justify-center px-2 py-4">
+            </m.div>
+            <m.div
+                initial={{ opacity: 0, y: 40 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.7, ease: 'easeOut', delay: 0.3 }}
+                className="col-span-3 row-span-2 row-start-7 bg-slate-600 rounded-2xl mt-36 h-60 bg-white/20 backdrop-blur-md border border-white/30 relative flex flex-col items-center justify-center px-2 py-4"
+            >
                 <h3 className="text-xl font-bold text-white mb-2 mt-3 text-center w-full z-10">Skills</h3>
                 <div className="w-full flex flex-col items-center justify-center gap-y-2">
                     {Skills.list.map((client) => (
@@ -128,18 +148,27 @@ const Mobile = () => {
                         </Carousel>
                     ))}
                 </div>
-            </div>
-            <div className="row-start-9 col-span-3 row-span-2  rounded-2xl h-52 mt-56 flex items-center justify-center">
+            </m.div>
+            <m.div
+                initial={{ opacity: 0, y: 40 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.7, ease: 'easeOut', delay: 0.4 }}
+                className="row-start-9 col-span-3 row-span-2  rounded-2xl h-52 mt-56 flex items-center justify-center"
+            >
                 <div className="w-full h-full grid grid-cols-2 grid-rows-2 gap-2">
                     {/* GitHub */}
-                    <div className="bg-slate-600 bg-white/20 backdrop-blur-md border border-white/30 rounded-xl flex items-center justify-center text-white font-semibold relative">
+                    <div 
+                    onClick={() => window.open('https://github.com/jawahendy', '_blank')}
+                    className="hover:cursor-pointer bg-slate-600 bg-white/20 backdrop-blur-md border border-white/30 rounded-xl flex items-center justify-center text-white font-semibold relative">
                         <span className="absolute top-1 right-1 bg-white/30 backdrop-blur-md rounded-full p-1 flex items-center justify-center">
                             <ArrowUpRight className='w-4 h-4 text-white' />
                         </span>
                         <svg xmlns="http://www.w3.org/2000/svg" className="w-7 h-7 text-white" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 6.5a5.07 5.07 0 0 0-.09-3.86S18.73 2.29 16 4a13.38 13.38 0 0 0-7 0C6.27 2.29 5.09 2.64 5.09 2.64A5.07 5.07 0 0 0 5 6.5a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" /></svg>
                     </div>
                     {/* LinkedIn */}
-                    <div className="bg-slate-600 bg-white/20 backdrop-blur-md border border-white/30 rounded-xl flex items-center justify-center text-white font-semibold relative">
+                    <div 
+                    onClick={() => window.open('https://www.linkedin.com/in/hendy-nurfrianto-4977b0177/', '_blank')}
+                    className="hover:cursor-pointer bg-slate-600 bg-white/20 backdrop-blur-md border border-white/30 rounded-xl flex items-center justify-center text-white font-semibold relative">
                         <span className="absolute top-1 right-1 bg-white/30 backdrop-blur-md rounded-full p-1 flex items-center justify-center">
                             <ArrowUpRight className='w-4 h-4 text-white' />
                         </span>
@@ -148,7 +177,9 @@ const Mobile = () => {
                         </svg>
                     </div>
                     {/* YouTube */}
-                    <div className="bg-slate-600 bg-white/20 backdrop-blur-md border border-white/30 rounded-xl flex items-center justify-center text-white font-semibold relative">
+                    <div 
+                    onClick={() => window.open('https://www.youtube.com/@Bang_hen490', '_blank')}
+                    className="hover:cursor-pointer bg-slate-600 bg-white/20 backdrop-blur-md border border-white/30 rounded-xl flex items-center justify-center text-white font-semibold relative">
                         <span className="absolute top-1 right-1 bg-white/30 backdrop-blur-md rounded-full p-1 flex items-center justify-center">
                             <ArrowUpRight className='w-4 h-4 text-white' />
                         </span>
@@ -157,7 +188,9 @@ const Mobile = () => {
                         </svg>
                     </div>
                     {/* Instagram */}
-                    <div className="bg-slate-600 bg-white/20 backdrop-blur-md border border-white/30 rounded-xl flex items-center justify-center text-white font-semibold relative">
+                    <div 
+                    onClick={() => window.open('https://www.instagram.com/hendy__n/', '_blank')}
+                    className="hover:cursor-pointer bg-slate-600 bg-white/20 backdrop-blur-md border border-white/30 rounded-xl flex items-center justify-center text-white font-semibold relative">
                         <span className="absolute top-1 right-1 bg-white/30 backdrop-blur-md rounded-full p-1 flex items-center justify-center">
                             <ArrowUpRight className='w-4 h-4 text-white' />
                         </span>
@@ -166,7 +199,7 @@ const Mobile = () => {
                         </svg>
                     </div>
                 </div>
-            </div>
+            </m.div>
         </div>
     );
 };
